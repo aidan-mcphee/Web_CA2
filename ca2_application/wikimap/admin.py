340d12b216aef9c95d398a255c1970e93fec0c0d
aidan-mcphee <iam@amcp.ie>
2025-12-07 22:40:28 +0000

    added admin page for Articles

--- ca2_application/wikimap/admin.py
+++ ca2_application/wikimap/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Article
+from django.core.management import call_command
+from django.contrib import messages
+import threading
 
 # Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.GISModelAdmin):
+    list_display = ('title', 'oldest_date', 'coordinates')
+    search_fields = ('title',)
+    list_filter = ('oldest_date',)
