0d294db82caf0ba26d9e2c9e910332170b125c25
aidan-mcphee <iam@amcp.ie>
2025-12-07 00:33:17 +0000

    added Article Model

--- ca2_application/wikimap/models.py
+++ ca2_application/wikimap/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
 
 # Create your models here.
+class Article(gis_models.Model):
+    title = models.CharField(max_length=200)
+    oldest_date = models.DateField()
+    coordinates = gis_models.PointField(srid=4326)
